233ede584442d918f8478bdfb542c1c3ca88271f
apple <giteshshastri96@gmail.com>
2019-02-11 02:05:55 +0530

    Added Responsive Page

---
+++ client/build/precache-manifest.b8c9fd495b90065db783658ed847f8f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69f877d41c3ed372c49738b586578d88",
+    "url": "/static/media/tab.69f877d4.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9e47ea1e0447d85e66af",
+    "url": "/static/js/main.9e47ea1e.chunk.js"
+  },
+  {
+    "revision": "58baa6f5276b1963095c",
+    "url": "/static/js/1.58baa6f5.chunk.js"
+  },
+  {
+    "revision": "9e47ea1e0447d85e66af",
+    "url": "/static/css/main.4ab320f4.chunk.css"
+  },
+  {
+    "revision": "ef4f4abba1464210076560e078058028",
+    "url": "/index.html"
+  }
+];
