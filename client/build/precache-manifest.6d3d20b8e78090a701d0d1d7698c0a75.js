0b2e3db7c7b2ccaca3504458822a02d3ad252ad7
apple <giteshshastri96@gmail.com>
2019-06-02 21:19:55 +0530

    Added Admin

---
+++ client/build/precache-manifest.6d3d20b8e78090a701d0d1d7698c0a75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157d2e404bfae01cbfe9bdc7fa0c9aca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e277492d8c2c4dc9de45",
+    "url": "/static/css/main.21de9465.chunk.css"
+  },
+  {
+    "revision": "4a3ed0243d848d8f3034",
+    "url": "/static/js/2.bef35a7d.chunk.js"
+  },
+  {
+    "revision": "e277492d8c2c4dc9de45",
+    "url": "/static/js/main.84338480.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b8c9fd495b90065db783658ed847f8f6.js"
+  "/precache-manifest.6d3d20b8e78090a701d0d1d7698c0a75.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
